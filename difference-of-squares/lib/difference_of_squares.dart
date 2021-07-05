9a9b5f1eb59e47d9c0e0306b691a98fa7d8ab851
ThunderFollower <44620256+ThunderFollower@users.noreply.github.com>
2021-07-05 16:56:29 +0300

    solutions adde

--- difference-of-squares/lib/difference_of_squares.dart
+++ difference-of-squares/lib/difference_of_squares.dart
@@ -1,3 +1,34 @@
+import 'dart:math';
+
 class DifferenceOfSquares {
-  // Put your code here
+
+  int squareOfSum(int n) {
+    return pow(_sumNatural(n), 2).toInt();
+  }
+
+  int _sumNatural(int n) => (n * (n + 1)) ~/ 2;
+
+  int sumOfSquares(int n) => (n * (n + 1) * ((2 * n) + 1)) ~/ 6;
+
+  int differenceOfSquares(int n) => squareOfSum(n) - sumOfSquares(n);
+
+  // int squareOfSum(int number) {
+  //   var result = 0;
+  //   for (int i = 1; i <= number; ++i) {
+  //     result += i;
+  //   }
+  //   return pow(result, 2).toInt();
+  // }
+  //
+  // int sumOfSquares(int number) {
+  //   var result = 0;
+  //   for (int i = 0; i <= number; ++i) {
+  //     result += pow(i, 2).toInt();
+  //   }
+  //   return result;
+  // }
+  //
+  // int differenceOfSquares(int number) {
+  //   return squareOfSum(number) - sumOfSquares(number) ;
+  // }
 }
